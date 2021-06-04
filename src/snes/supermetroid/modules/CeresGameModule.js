ae47d6595afcdbe9f3b9dffc8547c9df5be78eb9
Stashiocat <xx_sirhc_xx@yahoo.com>
2021-06-04 12:36:35 -0500

    Switch super super metroid games over to a more modular approach

---
+++ src/snes/supermetroid/modules/CeresGameModule.js
@@ -0,0 +1,40 @@
+import MemoryModule from '../../../util/memory/MemoryModule';
+import { Rooms, GameStates, PhantoonPatterns, CeresEscapeStateFlags } from '../enums';
+import Addresses from '../../addresses';
+
+const CeresGameState = {
+    Closed: 0,
+    Opened: 1,
+}
+
+export default class CeresGameModule extends MemoryModule {
+    constructor() {
+        super();
+        this.ceresState = CeresGameState.Closed;
+    }
+    async getMemoryReads() {
+        return [
+            Addresses.roomID,
+            Addresses.gameState,
+            Addresses.ceresTimer,
+            Addresses.ceresState,
+        ]
+    }
+    
+    async memoryReadAvailable(memory, handleEvent) {
+        if (this.ceresState == CeresGameState.Closed && this.checkTransition(memory.roomID, Rooms.EMPTY, Rooms.Ceres.CERES_ELEVATOR_ROOM)) {
+            handleEvent('ceresOpen');
+            this.ceresState = CeresGameState.Opened;
+        }
+        else if (this.ceresState == CeresGameState.Opened && this.checkTransition(memory.ceresState, CeresEscapeStateFlags.RIDLEY_SWOOP_CUTSCENE, CeresEscapeStateFlags.ESCAPE_TIMER_INITIATED)) {
+            handleEvent('ceresClose');
+            this.ceresState = CeresGameState.Opened;
+        }
+        else if (this.checkTransition(memory.gameState, [GameStates.BLACK_OUT_FROM_CERES, GameStates.CERES_ELEVATOR], GameStates.CERES_DESTROYED_CINEMATIC)) {
+            if (this.checkTransition(memory.gameState, [GameStates.BLACK_OUT_FROM_CERES, GameStates.CERES_ELEVATOR], GameStates.CERES_DESTROYED_CINEMATIC)) {
+                this.ceresState = CeresGameState.Closed;
+                handleEvent('ceresEnd', memory.ceresTimer.value);
+            }
+        }
+    }
+}
---
+++ src/util/memory/MemoryModule.js
@@ -0,0 +1,23 @@
+export default class MemoryModule {
+    constructor() {
+    }
+
+    async getMemoryReads() {
+        throw Error('You must implement getMemoryReads()');
+    }
+
+    async memoryReadAvailable(memory, handleEvent) {
+        throw Error('You must implement memoryReadAvailable()');
+    }
+
+    checkChange(read) {
+        return (read.value !== undefined && read.prevFrameValue === undefined) || (read.prevFrameValue !== undefined && read.value !== read.prevFrameValue);
+    }
+    
+    checkTransition(read, from, to) {
+        const fromTrue = Array.isArray(from) ? from.some((v) => v === read.prevFrameValue): read.prevFrameValue === from;
+        const toTrue = Array.isArray(to) ? to.some((v) => v === read.value): read.value === to;
+        return fromTrue && toTrue;
+    }
+}
+
