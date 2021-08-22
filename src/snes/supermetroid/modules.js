9535e244b8ea14d071aea9cf226ca9ab8bdcaf10
Tawling <tednewman111@gmail.com>
2021-08-22 13:41:33 -0700

    generic death handler

--- src/snes/supermetroid/modules.js
+++ src/snes/supermetroid/modules.js
@@ -5,9 +5,11 @@ import MoatDiveModule from './modules/MoatDive'
 import OceanDiveModule from './modules/OceanDive'
 import TacoTankTracker from './modules/TacoTankTracker'
 import ResetEventModule from './modules/ResetEvent'
+import DeathEventModule from './modules/DeathEvent'
 
 export const SuperMetroid = [
     ResetEventModule,
+    DeathEventModule,
     PhantoonGameModule,
     CeresGameModule,
     MoondanceEmoteOnlyModule,
