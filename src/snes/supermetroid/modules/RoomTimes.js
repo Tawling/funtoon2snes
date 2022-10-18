d7e07c8dce6551f7459f88c476141e10573a7d61
Tawling <tednewman111@gmail.com>
2022-10-17 18:03:20 -0700

    new room tracking system

--- src/snes/supermetroid/modules/RoomTimes.js
+++ src/snes/supermetroid/modules/RoomTimes.js
@@ -3,7 +3,7 @@ import { GameStates, Rooms } from "../enums";
 import Addresses from "../addresses";
 import { BossStates } from "../enums";
 import { noneOf, readBigIntFlag } from "../../../util/utils";
-import { isDemo } from "../smutils";
+import { counterDelta, isDemo, isIGTPaused } from "../smutils";
 
 const FPS = 60.098813897441;
 
@@ -11,7 +11,14 @@ export default class RoomTimes extends MemoryModule {
     constructor() {
         super("roomTimes", "Track Room Times", true, false);
         this.tooltip = "Tracks room times, sending events to FUNtoon for further processing.";
-        this.state = {};
+        this.resetState();
+    }
+
+    resetState() {
+        this.state = {
+            nmiRollover: 0,
+            frameCountRollover: 0,
+        };
     }
 
     shouldRunForGame(gameTags) {
@@ -54,14 +61,76 @@ export default class RoomTimes extends MemoryModule {
 
     memoryReadAvailable({ memory, sendEvent, globalState }) {
         if (isDemo(memory.gameState.value)) {
-            return
+            return;
         }
-        if (this.state.exit && this.checkChange(memory.roomID)) {
+
+        const isPaused = isIGTPaused(memory.gameState.value);
+        const wasPaused = isIGTPaused(memory.gameState.prevFrameValue);
+
+        const prevIGT =
+            memory.gameTimeHours.prevFrameValue * 216000 +
+            memory.gameTimeMinutes.prevFrameValue * 3600 +
+            memory.gameTimeSeconds.prevFrameValue * 60 +
+            memory.gameTimeFrames.prevFrameValue;
+
+        const currentIGT =
+            memory.gameTimeHours.value * 216000 +
+            memory.gameTimeMinutes.value * 3600 +
+            memory.gameTimeSeconds.value * 60 +
+            memory.gameTimeFrames.value;
+
+        if (memory.nmiCounter.value < memory.nmiCounter.prevFrameValue) {
+            this.state.nmiRollover++;
+        }
+
+        if (memory.frameCounter.value < memory.frameCounter.prevFrameValue) {
+            this.state.frameCountRollover++;
+        }
+
+        if (!wasPaused && isPaused) {
+            // IGT pause detected
+        } else if (wasPaused && !isPaused) {
+            // IGT unpause detected
+            if (
+                // Standard door entrance
+                this.checkTransition(memory.gameState, GameStates.LOAD_NEXT_ROOM_2, undefined) ||
+                // Ceres start / Zebes start
+                this.checkTransition(
+                    memory.gameState,
+                    [GameStates.NEW_GAME_POST_INTRO, GameStates.LOADING_GAME_DATA],
+                    [GameStates.INIT_GAME_AFTER_LOAD, GameStates.GAMEPLAY]
+                )
+            ) {
+                // Room entrance
+                const lag =
+                    counterDelta(memory.nmiCounter.prevFrameValue, memory.nmiCounter.value) -
+                    counterDelta(memory.frameCounter.prevFrameValue, memory.frameCounter.value);
+                const entryFrameDelta = currentIGT - prevIGT; // + lag;
+                console.log(entryFrameDelta, entryFrameDelta / FPS);
+                this.state = {
+                    entryNMI: memory.nmiCounter.value - entryFrameDelta,
+                    entryFrameCount: memory.frameCounter.value - entryFrameDelta,
+                    entryFrameDelta: entryFrameDelta,
+                    nmiRollover: 0,
+                    frameCountRollover: 0,
+                    entryTime: performance.now() - (entryFrameDelta / FPS) * 1000,
+                    prevRoomID: memory.roomID.prevUnique(),
+                    practiceEntryDelta: globalState.persistent.gameTags.PRACTICE ? memory.prRealtimeRoom.value : 0,
+                    entryIGT: prevIGT,
+                };
+            }
+        }
+
+        if (
+            this.state.exitTime &&
+            (this.checkChange(memory.roomID) ||
+                memory.gameState.value === GameStates.CERES_DESTROYED_CINEMATIC ||
+                memory.gameState.value === GameStates.BEAT_THE_GAME)
+        ) {
             // We got next room ID, time to send event
             const eventData = {
                 frameCount: this.state.totalFrames,
                 totalSeconds: this.state.roomTime,
-                trueFrames: this.state.trueFrames,
                 lagFrames: this.state.lagFrames,
                 igtFrames: this.state.igtFrames,
                 roomID: this.state.roomID,
@@ -84,112 +153,76 @@ export default class RoomTimes extends MemoryModule {
                 practiceFrames: globalState.persistent.gameTags.PRACTICE ? memory.prLastRealtimeRoom.value : 0,
                 practiceExitDelta: globalState.persistent.gameTags.PRACTICE ? memory.prTransitionCounter.value : 0,
                 practiceEntryDelta: this.state.practiceEntryDelta,
+                rps: globalState.readsPerSecond,
             };
             globalState.lastRoomTimeEvent = eventData;
             sendEvent("smRoomTime", eventData);
-            this.state = {};
+            this.resetState();
         } else if (
+            // Standard door exit
             this.checkTransition(
                 memory.gameState,
                 noneOf(GameStates.HIT_DOOR_BLOCK, GameStates.LOAD_NEXT_ROOM, GameStates.LOAD_NEXT_ROOM_2),
                 [GameStates.HIT_DOOR_BLOCK, GameStates.LOAD_NEXT_ROOM, GameStates.LOAD_NEXT_ROOM_2]
-            )
+            ) ||
+            // Ceres end
+            this.checkTransition(memory.gameState, undefined, GameStates.CERES_DESTROYED_CINEMATIC) ||
+            // Beat the game
+            this.checkTransition(memory.gameState, undefined, GameStates.BEAT_THE_GAME)
         ) {
             // Exiting room
-            const exitFrameDelta = this.getRoomExitTransitionFrame(memory);
-            if (exitFrameDelta === null) {
-                this.state = {};
-                return;
-            }
-            const timeDelta = (exitFrameDelta - 1) / FPS;
-            const exitTime = performance.now() - timeDelta * 1000;
-
-            if (this.state.entry) {
+            if (this.state.entryTime) {
                 // full room was tracked, log the prev room and time
 
-                const newLagDelta = memory.nmiCounter.value - memory.frameCounter.value;
-                const lagFrames = newLagDelta - this.state.lagDelta;
-                const currentIGT =
-                    memory.gameTimeHours.value * 216000 +
-                    memory.gameTimeMinutes.value * 3600 +
-                    memory.gameTimeSeconds.value * 60 +
-                    memory.gameTimeFrames.value;
+                const igtDelta = currentIGT - prevIGT;
+                const frameDelta = counterDelta(memory.frameCounter.prevFrameValue, memory.frameCounter.value);
+                const lag = counterDelta(memory.nmiCounter.prevFrameValue, memory.nmiCounter.value) - frameDelta;
 
-                const igtFrames = currentIGT - this.state.entryIGT;
-                const trueFrames = igtFrames + lagFrames;
+                console.log(igtDelta, frameDelta, lag);
 
-                const roomTime = exitTime - this.state.entry;
-                const totalFrames = Math.round((roomTime * FPS) / 1000); // Should this round in only one direction?
+                const exitFrameDelta = frameDelta - igtDelta; // + lag?
+                console.log(exitFrameDelta, (exitFrameDelta - 1) / FPS);
+                const exitTime = performance.now() - ((exitFrameDelta - 1) / FPS) * 1000;
+
+                const roomTime = exitTime - this.state.entryTime;
+
+                const roomTimeFrames = (roomTime * FPS) / 1000;
+                console.log("room time frames: ", roomTimeFrames);
+
+                const totalFrames =
+                    memory.nmiCounter.value + 65536 * this.state.nmiRollover - this.state.entryNMI - exitFrameDelta;
+                console.log("total frames: ", totalFrames);
+
+                console.log("delta: ", roomTimeFrames - totalFrames);
+
                 this.state.totalFrames = totalFrames;
-                this.state.exit = exitTime;
+                this.state.exitTime = exitTime;
                 this.state.roomID = memory.roomID.value;
                 this.state.roomTime = roomTime / 1000;
                 this.state.exitFrameDelta = exitFrameDelta;
 
-                this.state.lagFrames = lagFrames;
-                this.state.trueFrames = trueFrames;
+                this.state.exitNMI = memory.nmiCounter.value + 65536 * this.state.nmiRollover - exitFrameDelta;
+                this.state.exitFrameCount =
+                    memory.frameCounter.value + 65536 * this.state.frameCountRollover - exitFrameDelta;
+
+                this.state.lagFrames =
+                    this.state.exitNMI -
+                    this.state.entryNMI -
+                    (this.state.exitFrameCount - this.state.entryFrameCount) -
+                    lag;
                 this.state.exitIGT = currentIGT;
-                this.state.igtFrames = igtFrames
+                this.state.igtFrames = currentIGT - this.state.entryIGT;
+                console.log(this.state);
             } else {
                 // room wasn't tracked fully. We only have an exit time.
                 console.log("No room entrance time...skipping room time event");
             }
-        } else if (this.checkTransition(memory.gameState, GameStates.LOAD_NEXT_ROOM_2, undefined)) {
-            // Entering room
-            const entryFrameDelta = (memory.gameTimeFrames.value - memory.gameTimeFrames.prev() + 60) % 60; // mod 60 always wrapping to positive
-            this.storeRoomEntryState(memory, globalState, entryFrameDelta);
-        } else if (this.checkTransition(memory.roomID, Rooms.EMPTY, Rooms.Ceres.CERES_ELEVATOR_ROOM)) {
-            // CERES START???
-        } else if (this.checkTransition(
-            memory.gameState,
-            [GameStates.BLACK_OUT_FROM_CERES, GameStates.CERES_ELEVATOR],
-            GameStates.CERES_DESTROYED_CINEMATIC
-        )) {
-            // CERES END???
         }
-        // TODO: detect room start time on zebes after ceres cutscene
-        // TODO: detect room end for hitting ship at end of run
-        // TODO: detect starting run from ship instead of ceres
         // TODO: detect MB phase changes and send as separate timing event?
         // TODO: track baby skip jumps??
-    }
-
-    storeRoomEntryState(memory, globalState, entryFrameDelta) {
-        const timeDelta = (entryFrameDelta - 1) / FPS;
-        this.state = {
-            entry: performance.now() - timeDelta * 1000,
-            entryFrameDelta,
-            entryIGT:
-                memory.gameTimeHours.prev() * 216000 +
-                memory.gameTimeMinutes.prev() * 3600 +
-                memory.gameTimeSeconds.prev() * 60 +
-                memory.gameTimeFrames.prev(),
-            entryNMI: memory.nmiCounter.value - entryFrameDelta,
-            lagDelta: memory.nmiCounter.value - memory.frameCounter.value,
-            prevRoomID: memory.roomID.prevUnique(),
-            practiceEntryDelta: globalState.persistent.gameTags.PRACTICE ? memory.prRealtimeRoom.value : 0,
-        };
-    }
-
-    getRoomExitTransitionFrame(memory) {
-        if (memory.gameState.value === GameStates.HIT_DOOR_BLOCK) {
-            return 0;
-        } else if (memory.gameState.value === GameStates.LOAD_NEXT_ROOM) {
-            return 0;
-        } else {
-            if (memory.doorTransitionFunction.value < 0xe2db) {
-                return 1;
-            } else if (memory.doorTransitionFunction.value === 0xe2db) {
-                if (memory.paletteChangeNumerator.value === 1) {
-                    return 4;
-                } else if (memory.paletteIndex.value === 0x01fe) {
-                    return 2;
-                } else {
-                    return 3;
-                }
-            } else {
-                return null;
-            }
-        }
+        // TODO: track ammo usage
+        // TODO: track health changes
+        // TODO: track item pickups
+        // TODO: log door transition times
     }
 }
--- src/snes/supermetroid/smutils.js
+++ src/snes/supermetroid/smutils.js
@@ -43,3 +43,10 @@ export function isIGTPaused(gameState) {
         ].indexOf(gameState) < 0
     );
 }
+
+export function counterDelta(prevValue, newValue, bytes = 2) {
+    if (newValue < prevValue) {
+        return Math.pow(256, bytes) - prevValue + newValue;
+    }
+    return newValue - prevValue;
+}
