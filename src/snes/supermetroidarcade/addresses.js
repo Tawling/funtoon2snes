16f869f1f2ecd7a604891cbbde4eacd44b8c65d8
Stashiocat <xx_sirhc_xx@yahoo.com>
2023-11-17 15:20:15 -0600

    Updated comment

--- src/snes/supermetroidarcade/addresses.js
+++ src/snes/supermetroidarcade/addresses.js
@@ -1,16 +1,9 @@
 import { rom, wram } from "../datatypes";
 import MemState from "../../util/memory/MemState";
 
-/*
-    Achievement text: C6B0 - C6C7
-    - 1 byte for character, 1 byte for color
-
-    00C6BE - 248	Y
-    00C6C0 - 238	O
-    00C6C2 - 235	L
-    00C6C4 - 238	O
-    00C6C6 - 255	!
-*/
+// Achievement text: C6B0 - C6C7
+// 1 byte for character, 1 byte for color
+// up to 12 characters
 export default {
     arcadeSaveBehavior: new MemState(rom.uint8Read(0xa289), "arcadeSaveBehavior", "Super Metroid Arcade Save Behavior"),
     arcadeRoomCount: new MemState(wram.uint16Read(0x1FF90), "arcadeRoomCount", "Current Arcade Room Count"),
